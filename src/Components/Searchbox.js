33338c86e21faae344e5bfd188f4668bbdc8c372
genarene <genarene96@gmail.com>
2020-07-13 03:36:46 -0700

    initial commit of robofriends

---
+++ src/Components/Searchbox.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Searchbox =({searchField, searchChange})=>{
+    return(
+        <div className= 'pa2'>
+
+           <input 
+           className='pa3 ba b--green bg-lightest-blue'
+           type='Search' 
+           placeholder ='Search Robots'
+           onChange={searchChange}
+           />
+
+
+        </div>
+        
+    );
+}
+
+
+export default Searchbox
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
+
+import 'tachyons';
+import './Containers/App.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
